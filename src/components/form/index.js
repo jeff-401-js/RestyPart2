65d4cfb278e731a8724a396261a05ee5e5fb894c
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-14 16:00:02 -0700

    form working

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -3,15 +3,15 @@ import React from 'react';
 class Form extends React.Component {
   render(){
     return(
-      <form onSubmit={this.callAPI}>
+      <form onSubmit={this.props.callAPI}>
             <section>
               <input
                 type="text"
                 className="wide"
                 name="url"
                 placeholder="URL"
-                value={this.state.url}
-                onChange={this.handleChange}
+                value={this.props.url}
+                onChange={this.props.handleChange}
               />
 
               <div id="methods">
@@ -19,9 +19,9 @@ class Form extends React.Component {
                   <input
                     type="radio"
                     name="method"
-                    checked={this.state.method === 'get' ? true : false}
+                    checked={this.props.method === 'get' ? true : false}
                     value="get"
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                   />
                   <span>GET</span>
                 </label>
@@ -29,9 +29,9 @@ class Form extends React.Component {
                   <input
                     type="radio"
                     name="method"
-                    checked={this.state.method === 'post' ? true : false}
+                    checked={this.props.method === 'post' ? true : false}
                     value="post"
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                   />
                   <span>POST</span>
                 </label>
@@ -39,9 +39,9 @@ class Form extends React.Component {
                   <input
                     type="radio"
                     name="method"
-                    checked={this.state.method === 'put' ? true : false}
+                    checked={this.props.method === 'put' ? true : false}
                     value="put"
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                   />
                   <span>PUT</span>
                 </label>
@@ -49,9 +49,9 @@ class Form extends React.Component {
                   <input
                     type="radio"
                     name="method"
-                    checked={this.state.method === 'patch' ? true : false}
+                    checked={this.props.method === 'patch' ? true : false}
                     value="patch"
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                   />
                   <span>PATCH</span>
                 </label>
@@ -59,9 +59,9 @@ class Form extends React.Component {
                   <input
                     type="radio"
                     name="method"
-                    checked={this.state.method === 'delete' ? true : false}
+                    checked={this.props.method === 'delete' ? true : false}
                     value="delete"
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                   />
                   <span>DELETE</span>
                 </label>
@@ -76,43 +76,43 @@ class Form extends React.Component {
                 <textarea
                   placeholder="Raw JSON Body"
                   name="requestBody"
-                  onChange={this.handleChange}
-                  value={this.state.requestBody}
+                  onChange={this.props.handleChange}
+                  value={this.props.requestBody}
                   disabled={
-                    this.state.method.match(/get|delete/) ? true : false
+                    this.props.method.match(/get|delete/) ? true : false
                   }
                 />
               </div>
 
               <div id="headers">
-                <button onClick={this.toggleHeaders}>
+                <button onClick={this.props.toggleHeaders}>
                   Headers
                 </button>
-                <div className={'visible-' + this.state.headersVisible}>
+                <div className={'visible-' + this.props.headersVisible}>
                   <h2>Basic Authorization</h2>
                   <input
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                     name="username"
                     placeholder="Username"
-                    value={this.state.username}
+                    value={this.props.username}
                   />
                   <input
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                     name="password"
                     type="password"
                     placeholder="Password"
-                    value={this.state.password}
+                    value={this.props.password}
                   />
                 </div>
-                <div className={'visible-' + this.state.headersVisible}>
+                <div className={'visible-' + this.props.headersVisible}>
                   <h2>Bearer Token</h2>
                   <input
-                    onChange={this.handleChange}
+                    onChange={this.props.handleChange}
                     type="text"
                     className="wide"
                     name="token"
                     placeholder="Token"
-                    value={this.state.token}
+                    value={this.props.token}
                   />
                 </div>
               </div>
