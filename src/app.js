454401bfd5125c80825fb2d449ceedefc2fa8759
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-14 17:12:19 -0700

    store work

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import Header from './components/header/';
 import Footer from './components/footer/';
 import RESTy from './components/resty';
 
+// import * as actions from "./store/actions.js";
+
 class App extends React.Component {
   render() {
     return (
@@ -20,3 +22,38 @@ class App extends React.Component {
 }
 
 export default App;
+
+/**
+ * handler function to map state to props
+ * @param {object} state
+ * @description Sets the state properties
+ */
+
+// const mapStateToProps = state => ({
+//   data: state.restyStore
+// });
+
+/**
+ * handler function to dispatch state to props
+ * @param {function} dispatch
+ * @param {object} getState
+ * @description fires off the get or getOne functions
+ */
+
+// const mapDispatchToProps = (dispatch, getState) => ({
+//   get: () => dispatch(actions.getRemoteData()),
+//   getOne: payload => dispatch(actions.getRemotePerson(payload)),
+//   handlePost: payload => dispatch(actions.postRemote(payload)),
+//   handlePut: payload => dispatch(actions.putRemote(payload)),
+//   handleDelete: payload => dispatch(actions.deleteRemote(payload))
+// });
+
+/**
+ * export default connect
+ * @description export which contains the mapStateToProps and mapDispatchToProps
+ */
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(App);
