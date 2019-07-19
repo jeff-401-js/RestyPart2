3ca53c6ca2677f9e7449cf93a2237042a6f6dc74
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-19 15:24:19 -0700

    functinalizing

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 /**
- * Footer class component
+ * Footer function component
  * @description returns a simple footer that contains the copyright and author name
  */
 
-class Footer extends React.Component {
-  render() {
+function Footer () {
+
     return (
       <footer>
         <section>
@@ -14,7 +14,6 @@ class Footer extends React.Component {
         </section>
       </footer>
     );
-  }
 }
 
 export default Footer;
