e0e45c9a7d7863f0cede82f7aa986e287c1cf741
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-19 09:18:16 -0700

    store removed

--- src/app.js
+++ src/app.js
@@ -29,36 +29,3 @@ class App extends React.Component {
 
 export default App;
 
-/**
- * handler function to map state to props
- * @param {object} state
- * @description Sets the state properties
- */
-
-// const mapStateToProps = state => ({
-//   data: state.restyStore
-// });
-
-/**
- * handler function to dispatch state to props
- * @param {function} dispatch
- * @param {object} getState
- * @description fires off the get, handlePost, handlePut, or handleDelete functions
- */
-
-// const mapDispatchToProps = (dispatch, getState) => ({
-//   get: payload => dispatch(actions.getRemote(payload)),
-//   handlePost: payload => dispatch(actions.postRemote(payload)),
-//   handlePut: payload => dispatch(actions.putRemote(payload)),
-//   handleDelete: payload => dispatch(actions.deleteRemote(payload))
-// });
-
-/**
- * export default connect
- * @description export which contains the mapStateToProps and mapDispatchToProps
- */
-
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(App);
